59ba00130597a3d4b45a1317998b20d0d68792c3
dnniranjan <dnalininiranjan@hotmail.com>
2023-06-30 07:17:31 +0000

    modifiedcode

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Button = (props) => {
-  {content,class}=props;
-  return <button className={class}>{content}</button>
+  const { content, class1 } = props;
+  return <button className={class1}>{content}</button>;
 };
 
 const element = (
   <div className="bg_container">
     <h1 className="heading">Social Buttons</h1>
     <div className="button_container">
-      <Button class="like" content="Like"/>
-      <Button class="comment" content="Comment"/>
-      <Button class="share" content="Share"/>
+      <Button class1="like" content="Like" />
+      <Button class1="comment" content="Comment" />
+      <Button class1="share" content="Share" />
     </div>
   </div>
 );
